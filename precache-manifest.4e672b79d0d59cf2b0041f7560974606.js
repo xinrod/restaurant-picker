502f233f54a8aecf78d36bede07f944d2e844a4f
Roderick Liu <xinrod@live.unc.edu>
2020-09-13 22:05:28 -0400

    Updates

---
+++ precache-manifest.4e672b79d0d59cf2b0041f7560974606.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb62e22de5b4633cce94d761a453bf86",
+    "url": "/restaurant-picker/index.html"
+  },
+  {
+    "revision": "eb95989c8676e84f2301",
+    "url": "/restaurant-picker/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9f7075e83dda5cf4eb4e",
+    "url": "/restaurant-picker/static/css/main.6bfbf8bf.chunk.css"
+  },
+  {
+    "revision": "eb95989c8676e84f2301",
+    "url": "/restaurant-picker/static/js/2.45094f64.chunk.js"
+  },
+  {
+    "revision": "9f158e58f7171a81dc03e5fac0d8d1a5",
+    "url": "/restaurant-picker/static/js/2.45094f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7075e83dda5cf4eb4e",
+    "url": "/restaurant-picker/static/js/main.7c0e8dcc.chunk.js"
+  },
+  {
+    "revision": "1dd8ed3366b24a647254",
+    "url": "/restaurant-picker/static/js/runtime-main.39572f1a.js"
+  },
+  {
+    "revision": "bf84018890679b45a01356f47a6d6ca6",
+    "url": "/restaurant-picker/static/media/cool-background (2).bf840188.png"
+  }
+]);
